4767ae47464292fd6643e18c58a4cfaa2649bd46
Larissa Lopes <114454969+Lari-Lopes@users.noreply.github.com>
2022-11-29 17:10:15 -0300

    Add files

---
+++ src/js/index.js
@@ -0,0 +1,62 @@
+/*
+
+OBJETIVO 1 - quando clicarmos na seta de avançar temos que mostrar o próximo cartão da lista 
+       - passo 1 - dar um jeito de pegar o elemento HTML da seta avançar 
+       - passo 2 - dar um jeito de identificar o clique do usuário na seta avançar 
+       - passo 3 - fazer aparecer o cartão próximo da lista 
+       - passo 4 - buscar o cartão que está selecionado e esconder 
+*/ 
+
+//  passo 1 - dar um jeito de pegar o elemento HTML da seta avançar 
+const btnAvancar = document.getElementById("btn-avancar");
+const btnVoltar = document.getElementById("btn-voltar");
+const cartoes = document.querySelectorAll(".cartao");
+let cartaoAtual = 0;
+
+
+function esconderCartaoSelecionado(){
+    const cartaoSelecionado = document.querySelector(".selecionado");
+    cartaoSelecionado.classList.remove("selecionado");
+
+}
+
+function mostrarCartao(indiceCartaoPraMostrar) {
+    cartoes[indiceCartaoPraMostrar].classList.add("selecionado");
+}
+function mostrarCartao (indiceCartao) {
+    cartoes[indiceCartao].classList.add("selecionado");
+
+}
+// passo 2 - dar um jeito de identificar o clique do usuário na seta avançar
+btnAvancar.addEventListener("click", function () {
+
+     if(cartaoAtual === cartoes.length -1) return;
+     
+     esconderCartaoSelecionado();
+
+    cartaoAtual++;
+    mostrarCartao(cartaoAtual);
+
+});
+
+/*
+OBJETIVO 2 - quando clicarmos na seta de voltar temos que mostrar o cartão anterior da lista 
+       - passo 1 - dar um jeito de pegar o elemento HTML da seta voltar
+       - passo 2 - dar um jeito de identificar o clique do usuário na seta voltar 
+       - passo 3 - fazer aparecer o cartão anterior da lista
+       - passo 4 - buscar o cartão que está selecionado e esconder
+
+*/
+
+btnVoltar.addEventListener("click", function () {
+
+    if(cartaoAtual === 0) return;
+
+    esconderCartaoSelecionado();
+    
+    cartaoAtual--;
+    mostrarCartao(cartaoAtual);
+
+});
+
+
